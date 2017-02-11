b71a546d5e11cf5976b839d799cc49c1e0fb5b4b
kaar3l <p1ngv11n>
2017-02-11 16:11:48 +0200

    Second

---
+++ index.php
@@ -0,0 +1,13 @@
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<html>
+<title>Kaarli Leht</title>
+<body><p>Lorem Ipsum</p> <h1>is simply dummy text</h1> of the printing and typesetting industry. <br>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br>
+<img src="bicycle.jpg" alt="See on minu ratas!" />
+<br>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</body>
+<br>
+<br>
+<?php echo 'Current PHP version: ' . phpversion(); ?>
+<meta charset="UTF-8" />
+<!DOCTYPE html>
