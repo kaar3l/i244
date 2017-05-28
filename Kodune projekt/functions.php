c0454a8077aa2f49c116b2ec424b5daf7e328647
Kaarel <kaarel@Gigabyte>
2017-05-28 23:29:41 +0300

    Koodipuhastus

--- Kodune projekt/functions.php
+++ Kodune projekt/functions.php
@@ -55,68 +55,88 @@ function lisa_kulurida(){
 	//Kasutajakontroll
 	if(!empty($_SESSION['user'])){
 		//On ilusti kasutajaga sees
+		if(!empty($_SESSION['admin'])){
+			//On ka admin järelikult saab ridu lisada
+
+			//Lisab kuluridadesse ridu juurde vormi seest
+			global $connection;
+
+			include_once('views/reasisestus.html');
+
+			if(empty($_POST['date']) and empty($_POST['liik']) and empty($_POST['summa'])) {
+
+			} else {
+				$kuupaev = mysqli_real_escape_string($connection,$_POST['date']);
+				$liik = mysqli_real_escape_string($connection,$_POST['liik']);
+				//koma sisestamisel vahetame punkti vastu
+				$summapunktiga=str_replace(',', '.', $_POST['summa']);
+				$summa = mysqli_real_escape_string($connection,$summapunktiga);
+				$m2rkus = mysqli_real_escape_string($connection,$_POST['m2rkus']);
+				$sql = "INSERT INTO `kulutused`.`kulud` (aeg, liik, summa, kommentaar) VALUES ('$kuupaev', $liik, $summa, '$m2rkus')";
+				if (mysqli_query($connection, $sql)) {
+					echo "Uus rida lisatud edukalt";
+					header("location: kulud.php?page=kuluread");
+				} else {
+					echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+				}
+				mysqli_close($conn);
+			}
+		}else{
+				//Pole admin, seega viskame kulude vaatamisse
+				header("location: kulud.php?page=kuluread");
+		};
 	}else{
 		header("location: kulud.php?page=login");
 	};
 
-		//Lisab kuluridadesse ridu juurde vormi seest
-		global $connection;
 
-		include_once('views/reasisestus.html');
-
-		if(empty($_POST['date']) and empty($_POST['liik']) and empty($_POST['summa'])) {
-
-		} else {
-			$kuupaev = mysqli_real_escape_string($connection,$_POST['date']);
-			$liik = mysqli_real_escape_string($connection,$_POST['liik']);
-			//koma sisestamisel vahetame punkti vastu
-			$summapunktiga=str_replace(',', '.', $_POST['summa']);
-			$summa = mysqli_real_escape_string($connection,$summapunktiga);
-			$m2rkus = mysqli_real_escape_string($connection,$_POST['m2rkus']);
-			$sql = "INSERT INTO `kulutused`.`kulud` (aeg, liik, summa, kommentaar) VALUES ('$kuupaev', $liik, $summa, '$m2rkus')";
-			if (mysqli_query($connection, $sql)) {
-				echo "Uus rida lisatud edukalt";
-				header("location: kulud.php?page=kuluread");
-			} else {
-				echo "Error: " . $sql . "<br>" . mysqli_error($connection);
-			}
-			mysqli_close($conn);
-		}
 }
 
 function lisa_kululiike(){
 	//Kasutajakontroll
 	if(!empty($_SESSION['user'])){
 		//On ilusti kasutajaga sees
+		//On ilusti kasutajaga sees
+		if(!empty($_SESSION['admin'])){
+			//On ka admin järelikult saab ridu lisada
+
+			//Lisab kululiike juurde
+			//Näitab kululiigid
+				global $connection;
+				display_kululiigid();
+
+				include_once('views/liigisisestus.html');
+
+				if(empty($_POST['liik'])) {
+					//Do nothing
+				} else {
+					$liik = mysqli_real_escape_string($connection,$_POST['liik']);
+					$m2rkus = mysqli_real_escape_string($connection,$_POST['m2rkus']);
+					$sql = "INSERT INTO `kulutused`.`kululiigid` (liik, kommentaar) VALUES ('$liik', '$m2rkus')";
+					echo $_POST['liik'];
+					echo "<br>";
+					echo $_POST['m2rkus'];
+					echo "<br>";
+					if (mysqli_query($connection, $sql)) {
+						echo "Uus rida lisatud edukalt";
+						header("location: kulud.php?page=lisaliike");
+					} else {
+						echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+					}
+					mysqli_close($conn);
+				}
+
+
+		}else{
+			//Pole admin, seega viskame kulude vaatamisse
+			header("location: kulud.php?page=kuluread");
+		};
+
 	}else{
 		header("location: kulud.php?page=login");
 	};
-	
-	//Lisab kululiike juurde
-	//Näitab kululiigid
-		global $connection;
-		display_kululiigid();
 
-		include_once('views/liigisisestus.html');
 
-		if(empty($_POST['liik'])) {
-			//Do nothing
-		} else {
-			$liik = mysqli_real_escape_string($connection,$_POST['liik']);
-			$m2rkus = mysqli_real_escape_string($connection,$_POST['m2rkus']);
-			$sql = "INSERT INTO `kulutused`.`kululiigid` (liik, kommentaar) VALUES ('$liik', '$m2rkus')";
-			echo $_POST['liik'];
-			echo "<br>";
-			echo $_POST['m2rkus'];
-			echo "<br>";
-			if (mysqli_query($connection, $sql)) {
-				echo "Uus rida lisatud edukalt";
-				header("location: kulud.php?page=lisaliike");
-			} else {
-				echo "Error: " . $sql . "<br>" . mysqli_error($connection);
-			}
-			mysqli_close($conn);
-		}
 }
 
 function display_kululiigid(){
@@ -256,135 +276,4 @@ function generate_piechart($input){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function muuda_looma(){
-	//ADMINI kontroll:
-	if($_SESSION['admin']==1){
-		global $connection;
-		//KASUTAJA kontroll:
-		if(!empty($_SESSION['user'])){
-			echo "Oled kenasti sisse logitud kasutaja: ";
-			echo $_SESSION['user'];
-		}else{
-			header("location: loomaaed.php?page=login");
-		}
-
-		//Muudetava looma id:
-		$muudetavaLoomaID=$_GET['id'];
-
-
-		//get_loom - võtab andmebaasist looma info rea
-		$loom=get_loom($muudetavaLoomaID);
-		//display_loom - echotab looma info nähtavaks
-		display_loom($loom);
-
-//		echo $_SESSION['muudetavaLoomaID'];
-		echo "<br>";
-
-		if(empty($_POST['nimi']) and empty($_POST['puur'])) {
-			echo "<b>Muuda looma:</b>";
-		} else {
-			$loomaID=$_GET['id'];
-			$nimi = mysqli_real_escape_string($connection,$_POST['nimi']);
-			$puur = mysqli_real_escape_string($connection,$_POST['puur']);
-			$yleslaad=$_FILES["liik"]["ylesLaetavFail"];
-
-			$uploadResult=upload("liik");
-
-			$loomaID=$_SESSION['muudetavaLoomaID'];
-			echo "<br><br>";
-//			echo "LoomaID: ";
-//			echo $loomaID;
-//			echo "<br><br>";
-			$sql = "UPDATE loomad SET nimi='$nimi', puur=$puur, liik='$uploadResult' WHERE id=$loomaID";
-//			echo $loomaID;
-
-			if (mysqli_query($connection, $sql)) {
-				echo "Loom muudetud";
-				header("location: loomaaed.php?page=muuda&id=$loomaID");
-			} else {
-				echo "Error: " . $sql . "<br>" . mysqli_error($connection);
-			}
-			mysqli_close($conn);
-		}
-		//Salvestame nüüd loomaid ära enne uue lehe laadimist, et teaks mis looma uuendada
-		$_SESSION['muudetavaLoomaID']=$muudetavaLoomaID;
-
-		include_once('views/editvorm.html');
-	} else {
-		//Kuna sa poel admin, siis viskame loomaaia vaaatesse
-		header("location: loomaaed.php?page=loomad");
-	}
-}
-
-function get_loom($input_id){
-	global $connection;
-	$sql="SELECT * FROM loomad WHERE id=($input_id)";
-	$result = mysqli_query($connection,$sql);
-	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	return $row;
-}
-
-function display_loom($input_loom){
-	echo "<br>";
-	echo "<b>Praegune looma info:</b>";
-	echo "<br>";
-//	echo "id: " . $input_loom["id"];
-//	echo "<br>";
-	echo "nimi: " . $input_loom["nimi"];
-	echo "<br>";
-	echo "puur: " . $input_loom["puur"];
-	echo "<br>";
-	echo "liik: " . $input_loom["liik"];
-	echo "<br>";
-	echo "<img src=" . $input_loom["liik"] . ">";
-}
-
-
-
-function upload($name){
-	$allowedExts = array("jpg", "jpeg", "gif", "png");
-	$allowedTypes = array("image/gif", "image/jpeg", "image/png","image/pjpeg");
-	$extension = end(explode(".", $_FILES[$name]["name"]));
-
-	if ( in_array($_FILES[$name]["type"], $allowedTypes)
-		&& ($_FILES[$name]["size"] < 100000)
-		&& in_array($extension, $allowedExts)) {
-    // fail õiget tüüpi ja suurusega
-		if ($_FILES[$name]["error"] > 0) {
-			$_SESSION['notices'][]= "Return Code: " . $_FILES[$name]["error"];
-			return "";
-		} else {
-      // vigu ei ole
-			if (file_exists("pildid/" . $_FILES[$name]["name"])) {
-        // fail olemas ära uuesti lae, tagasta failinimi
-				$_SESSION['notices'][]= $_FILES[$name]["name"] . " juba eksisteerib. ";
-				return "pildid/" .$_FILES[$name]["name"];
-			} else {
-        // kõik ok, aseta pilt
-				move_uploaded_file($_FILES[$name]["tmp_name"], "pildid/" . $_FILES[$name]["name"]);
-				return "pildid/" .$_FILES[$name]["name"];
-			}
-		}
-	} else {
-		return "";
-	}
-}
-/*
 ?>
